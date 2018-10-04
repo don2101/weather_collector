e5878f507de292ba636070485f4aa613603aaef0
Prower <don210198@gmail.com>
2018-10-04 22:29:55 +0900

    * Xml 파싱 기능 추가  - Get으로 가져온 Xml을 Document로 변환  - Document의 <data> tag를 찾아 내부의 데이터를 가져와 전송
      ? 데이터 전송방식 어떻게 할지 결정
      ? 추가 데이터 추출을 어디에서 할지 결정

--- src/main/java/astro/api/collector/api/WeatherApi.java
+++ src/main/java/astro/api/collector/api/WeatherApi.java
@@ -3,6 +3,17 @@ package astro.api.collector.api;
 import astro.api.collector.common.HttpManager;
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.exceptions.UnirestException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+import java.io.StringReader;
 
 public class WeatherApi implements ApiInterface {
 
@@ -25,6 +36,45 @@ public class WeatherApi implements ApiInterface {
     }
 
     public String parse(String responseBody) {
-        return responseBody;
+        String message = "";
+        InputSource inputSource = new InputSource(new StringReader(responseBody));
+
+        try {
+            DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+            Document document = documentBuilder.parse(inputSource);
+
+            NodeList dataList = document.getElementsByTagName("data");
+
+            /* 전체 데이터 전송
+            for(int i = 0; i < dataList.getLength(); ++i) {
+                Node data = dataList.item(i);
+                NodeList weatherDataList = data.getChildNodes();
+
+                for(int j = 0; j < weatherDataList.getLength(); ++j) {
+                    Node weatherData = weatherDataList.item(j);
+                    message += weatherData.getTextContent();
+                    message += "_";
+                }
+            }*/
+
+            Node data = dataList.item(0);
+
+            NodeList weatherDataList = data.getChildNodes();
+            for(int i = 0; i < weatherDataList.getLength(); ++i) {
+                Node weatherData = weatherDataList.item(i);
+                message += weatherData.getTextContent();
+            }
+
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        return message;
     }
 }
